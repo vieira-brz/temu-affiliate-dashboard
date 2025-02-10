313554d0284328b5d3f2600fd55e8d038a86a4f4
vieira-brz <vieirabraz2003@gmail.com>
2025-02-10 13:00:23 -0300

    .

--- server.js
+++ server.js
@@ -49,5 +49,5 @@ app.post("/items", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em https://temu-affiliate-dashboard-k1qmm6t7d-vinicius-brazs-projects.vercel.app:${PORT}`);
+    console.log(`Servidor rodando em https://shoplinkbr.vercel.app:${PORT}`);
 });
